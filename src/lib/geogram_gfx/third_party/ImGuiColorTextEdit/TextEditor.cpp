a0db80cfccb637451c2875f3b6c960892e9066e4
Bruno Levy <Bruno.Levy@inria.fr>
2022-11-23 19:34:32 +0100

    Removed compatibility with Visual C++ 2015 (we are in 2022 !!)

--- src/lib/geogram_gfx/third_party/ImGuiColorTextEdit/TextEditor.cpp
+++ src/lib/geogram_gfx/third_party/ImGuiColorTextEdit/TextEditor.cpp
@@ -51,35 +51,6 @@ bool equals(InputIt1 first1, InputIt1 last1,
 }
 
 
-// [Bruno Levy] snprintf and vsnprintf for old MSVC compilers
-
-#if defined(_MSC_VER) && _MSC_VER < 1900
-#   define snprintf c99_snprintf
-#   define vsnprintf c99_vsnprintf
-
-namespace {
-    __inline int c99_vsnprintf(char *outBuf, size_t size, const char *format, va_list ap) {
-	int count = -1;
-	
-	if (size != 0)
-	    count = _vsnprintf_s(outBuf, size, _TRUNCATE, format, ap);
-	if (count == -1)
-	    count = _vscprintf(format, ap);
-	return count;
-    }
-    
-    __inline int c99_snprintf(char *outBuf, size_t size, const char *format, ...) {
-	int count;
-	va_list ap;
-	va_start(ap, format);
-	count = c99_vsnprintf(outBuf, size, format, ap);
-	va_end(ap);
-	return count;
-    }
-}
-#endif    
-
-
 // [Bruno Levy] functions for management of HighDPI displays.
 namespace {
 #if defined(__EMSCRIPTEN__) || defined(__ANDROID__)
