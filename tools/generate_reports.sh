2698fcadf3191c63be9f1b0c41dc3d1bc1450a51
Bruno Levy <Bruno.Levy@inria.fr>
2022-12-04 08:00:29 +0100

    Redesigned continuous action, now publication to gh-pages is done in a separate job.

---
+++ tools/generate_reports.sh
@@ -0,0 +1,13 @@
+# generate_report.sh
+# generates test reports and dashboard from github action artifacts
+# (artifacts are zip files with test results)
+
+mkdir reports
+
+for artifact in `ls *.zip`
+do
+  dirname=`basename $artifact .zip`
+  mkdir reports/$dirname
+  (cd reports/$dirname; unzip ../../$artifact)
+done
+		
