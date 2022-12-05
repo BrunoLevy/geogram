ee6c559696ec5b197c7562f864b7c60a3d5ec9f6
Bruno Levy <Bruno.Levy@inria.fr>
2022-12-05 17:22:14 +0100

    Reorganization of report/dashboard script. Now generates link to build log, and warnings report.

--- tools/generate_reports.sh
+++ tools/generate_reports.sh
@@ -16,18 +16,26 @@ done
 for category in `ls reports`
 do
     output=reports/$category/index.html
-    echo > $output
+    cat tools/head.html > $output
+    echo "<body>" >> $output
     echo "<H1> " >> $output
     echo "<img width=\"40\" style=\"vertical-align:middle\" "\
          "src=\"Images/robot.png\"/>" >> $output
-    echo "$category tests - Robot Framework reports </H1>" >> $output
+    echo "$category tests - Dashboard </H1>" >> $output
     echo "</H1>" >> $output
-    echo "<ul>" >> $output
+    echo "<table>" >> $output
+    echo "<tr> " >> $output
+    echo "<th> Configuration </th>" >> $output
+    echo "<th> Robot </th>" >> $output
+    echo "<th> Build </th>" >> $output            
+    echo "</tr>" >> $output
     for config in `ls reports/$category`
     do
 	if [[ -d "reports/$category/$config" ]]
 	then
-	    echo "<li>" >> $output
+	    echo "<tr>" >> $output
+	    echo "<td> $config </td>" >> $output
+	    echo "<td>" >> $output
 	    if [[ -f "reports/$category/$config/TESTS_SUCCESS" ]]
 	    then
 		echo "OK $category/$config"
@@ -39,11 +47,19 @@ do
 		     "src=\"Images/ko.png\"/>" >> $output
 	    fi
 	    echo "&nbsp;" >> $output
-	    echo "<a href=\"$config/report.html\">$config</a>" >> $output
-	    echo "</li>" >> $output
+	    echo "<a href=\"$config/report.html\">report</a>" >> $output
+	    echo "</td>" >> $output
+	    echo "<td>"  >> $output
+	    echo "<a href=\"$config/build_log.txt\">log</a>" >> $output
+	    echo "&nbsp" >> $output
+	    echo `cat reports/$category/$config/build_log.txt |\
+                  grep -i "warning" | wc -l` "warning(s)" >> $output
+	    echo "</td>" >> $output
+	    echo "</tr>" >> $output
 	fi
     done
-    echo "</ul>" >> $output
+    echo "</table>" >> $output
+    echo "</body>" >> $output    
     # comes last, so that Images is ignored in iteration above
     cp -r tools/Images reports/$category
 done
