1846568797c01e8d486127af9c657889d397068f
Bruno Levy <Bruno.Levy@inria.fr>
2023-01-09 10:40:34 +0100

    Cedric's patch: on mesh file load, change working directory to directory.

--- src/lib/geogram/mesh/mesh_io.cpp
+++ src/lib/geogram/mesh/mesh_io.cpp
@@ -4800,7 +4800,11 @@ namespace GEO {
         }
 
         M.show_stats("I/O");
-        
+
+        const std::string dir = FileSystem::dir_name(filename);
+        if( FileSystem::is_directory(dir) ) {
+            FileSystem::set_current_working_directory(dir);
+        }
         return true;
     }
 
