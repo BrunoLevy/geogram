ddacc7013124fbb1b8e4514f8d9613bf48d2d95e
Bruno Levy <Bruno.Levy@inria.fr>
2023-06-26 10:33:57 +0200

    - MeshHalfedges: function for taking region into account was confusing, with an implicit conversion to bool. Created another overload to disambiguate. - Preparing Release 1.8.4

--- src/lib/geogram/mesh/mesh_halfedges.h
+++ src/lib/geogram/mesh/mesh_halfedges.h
@@ -179,6 +179,17 @@ namespace GEO {
 	    facet_region_.bind(mesh_.facets.attributes(),attribute_name);
 	}
 
+	/**
+	 * \brief Sets a facet attribute name that determines borders.
+	 * \param[in] attribute_name the name of the facet attribute to
+	 *  be used to determine borders.
+         * \details Needed to have this overload, because const char*
+         *  is implicitly converted to bool instead of std::string.
+	 */
+        void set_use_facet_region(const char* attribute_name) {
+            set_use_facet_region(std::string(attribute_name));
+        }
+        
 	
         /**
          * \brief Tests whether a Halfedge is valid.
