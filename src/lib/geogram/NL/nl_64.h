d038b371b81c8e4c8b70bcd5954c6e1ca6ca300e
Bruno <Bruno.Levy@inria.fr>
2024-10-25 15:11:17 +0200

    Ooops, nlSetIJCoefficientAtRowOffset was calling AddIJCoefficientAt() in nl_64.h (fixed)

--- src/lib/geogram/NL/nl_64.h
+++ src/lib/geogram/NL/nl_64.h
@@ -91,7 +91,7 @@ inline void nlAddIJCoefficientAt(
 inline void nlSetIJCoefficientAtRowOffset(
     NLulong i, NLulong j, NLdouble a, NLulong row_offset
 ) {
-    nlAddIJCoefficientAt(nlTo32(i), nlTo32(j), a, nlTo32(row_offset));
+    nlSetIJCoefficientAtRowOffset(nlTo32(i), nlTo32(j), a, nlTo32(row_offset));
 }
 
 inline double nlGetEigenValue(int i) {
