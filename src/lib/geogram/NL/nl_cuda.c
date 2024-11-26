5612a9413b6ab1b4d61f42a8639729ef23bad5a1
Bruno <Bruno.Levy@inria.fr>
2024-11-26 07:28:12 +0100

    Added missing entries in ConvertSMVer2Cores()

--- src/lib/geogram/NL/nl_cuda.c
+++ src/lib/geogram/NL/nl_cuda.c
@@ -708,13 +708,16 @@ static int ConvertSMVer2Cores(int major, int minor) {
         { 0x30, 192}, /* Kepler Generation  (SM 3.0) GK10x class  */
         { 0x32, 192}, /* Kepler Generation  (SM 3.0) */
         { 0x35, 192}, /* Kepler Generation  (SM 3.5) GK11x class  */
+	{ 0x37, 192},
         { 0x50, 128}, /* Maxwell Generation (SM 5.0) GM10x class
                          (yes, #cores smaller than with 3.x)  */
         { 0x52, 128}, /* Maxwell Generation (SM 5.2) GM20x class  */
+        { 0x53, 128},
         { 0x60, 64 }, /* Pascal Generation  (SM 6.0) GP100,GP102
                          (yes, 64, but GP100 has superfast double precision) */
         { 0x61, 128}, /* Pascal Generation  (SM 6.1) GP104 class
                          (but FP64 runs as 1/32 FP32 speed) */
+	{ 0x62, 128},
         { 0x70, 64 }, /* Volta Generation (SM 7.0)
 		         yes, nb cores decreased in SM 7.x  */
         { 0x72, 64 }, /* Volta Generation (SM 7.2) */
