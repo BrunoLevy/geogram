2237bd656679af41d0a5e46b18084ad4cc0cbcae
Bruno <Bruno.Levy@inria.fr>
2025-02-10 08:27:57 +0100

    under MacOS...)

--- src/lib/geogram/third_party/zlib/zconf.h
+++ src/lib/geogram/third_party/zlib/zconf.h
@@ -20,8 +20,9 @@
 #endif
 
 #if defined(__APPLE__)
-#include <sys/types.h>
-typedef off_t off64_t; /* On MacOS, file IO is 64 bits by default ! */
+/* On MacOS, file IO is 64 bits by default ! */
+#define off64_t off_t
+#define lseek64 lseek
 #endif
 
 /*
