80c4710063ead514726c0fc36fa744a83f47d289
Bruno <Bruno.Levy@inria.fr>
2025-11-21 11:57:00 +0100

    add a default in switch statemet (fixing warnings detected my clang/MacOS, but it may trigger other warnings in other compilers, we shall see...)

--- src/lib/geogram/basic/attributes.h
+++ src/lib/geogram/basic/attributes.h
@@ -2060,6 +2060,8 @@ namespace GEO {
                 break;
             case ET_NONE:
                 geo_assert_not_reached;
+	    default:
+                geo_assert_not_reached;
             }
             return result;
         }
@@ -2116,6 +2118,8 @@ namespace GEO {
                 break;
             case ET_NONE:
                 geo_assert_not_reached;
+	    default:
+		geo_assert_not_reached;
             }
             return result;
         }
