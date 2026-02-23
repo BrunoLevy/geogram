ad1ca847ec50d1ed0d0254d26a774f6d6e1b7d6e
Bruno Levy <Bruno.Levy@inria.fr>
2026-02-23 22:13:07 +0100

    cleanup, mode

--- src/lib/geogram_gfx/GLUP/GLUP.h
+++ src/lib/geogram_gfx/GLUP/GLUP.h
@@ -153,7 +153,8 @@ extern "C" {
         GLUP_PICKING             =7,
         GLUP_ALPHA_DISCARD       =8,
         GLUP_NORMAL_MAPPING      =9,
-        GLUP_PRIMITIVE_FILTERING =10
+        GLUP_PRIMITIVE_FILTERING =10,
+	GLUP_TRANSPARENT         =11
     } GLUPtoggle;
 
     void GLUP_API glupEnable(GLUPtoggle toggle);
