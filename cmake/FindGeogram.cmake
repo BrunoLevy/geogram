c6622893ec17b5f9683b13a7e03a205edcbd7866
Zoltan Csati <CsatiZoltan@users.noreply.github.com>
2026-08-31 11:20:00 +0200

    Do not explicitly provide the lib/ search path in FindGeogram.cmake
    On 64 bit systems, geogram could be installed in *lib64/*, not *lib/*. CMake recognizes it automatically, searching both in *lib/* and in *lib64/*. However, it is no longer the case if the `PATH_SUFFIXES` is given. In this case, the geogram library is not found in *lib64/*.

--- cmake/FindGeogram.cmake
+++ cmake/FindGeogram.cmake
@@ -40,13 +40,11 @@ find_path (GEOGRAM_INCLUDE_DIR
 find_library (GEOGRAM_LIBRARY
                 NAMES geogram
                 PATHS ${GEOGRAM_SEARCH_PATHS}
-                PATH_SUFFIXES lib
 )
 
 find_library (GEOGRAM_GFX_LIBRARY
                 NAMES geogram_gfx
                 PATHS ${GEOGRAM_SEARCH_PATHS}
-                PATH_SUFFIXES lib
 )
 
 # This one we search in both Geogram search path and
@@ -55,7 +53,6 @@ find_library (GEOGRAM_GFX_LIBRARY
 find_library (GEOGRAM_GLFW3_LIBRARY
                 NAMES glfw3 glfw geogram_glfw3 glfw3dll glfwdll
                 PATHS ${GEOGRAM_SEARCH_PATHS} ${GEOGRAM_SEARCH_PATHS_SYSTEM}
-                PATH_SUFFIXES lib
 )
 
 include (FindPackageHandleStandardArgs)
